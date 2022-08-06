f9c1deb87e7d06a42347406fe5f16b9b408087a6
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2022-08-06 18:44:16 -0400

    s02-33 Anonymous Functions

--- lib/main.dart
+++ lib/main.dart
@@ -34,11 +34,14 @@ class MyApp extends StatelessWidget {
             ),
             RaisedButton(
               child: Text('Answer 2'),
-              onPressed: answerQuestion,
+              onPressed: () => print('Answer 2 chosen!'),
             ),
             RaisedButton(
               child: Text('Answer 3'),
-              onPressed: answerQuestion,
+              onPressed: () {
+                // ...
+                print('Answer 3 chosen!');
+              },
             ),
           ],
         ),
